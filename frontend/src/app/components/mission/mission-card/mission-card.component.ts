f6ed420f94805e35144c8823fa0afa7e293a3b96
Paco Poyo-Dufour <96145167+PoyPoyyyy@users.noreply.github.com>
2025-01-14 16:49:50 +0100

    Add - File structure

---
+++ frontend/src/app/components/mission/mission-card/mission-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mission-card',
+  imports: [],
+  templateUrl: './mission-card.component.html',
+  styleUrl: './mission-card.component.css'
+})
+export class MissionCardComponent {
+
+}
