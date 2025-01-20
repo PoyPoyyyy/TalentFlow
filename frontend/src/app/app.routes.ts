2dc7f63b07b0ac6dfef5646eb4f0239e0d0143e6
Baptiste731 <baptiste.terron@gmail.com>
2025-01-20 15:11:18 +0100

    Add - Page not found

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {EmployeePageComponent} from './components/employee/employee-page/employee-page.component';
 import {MissionPageComponent} from './components/mission/mission-page/mission-page.component';
 import {SkillPageComponent} from './components/skill/skill-page/skill-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {path: 'employee-page', component: EmployeePageComponent},
   {path: 'mission-page', component: MissionPageComponent},
-  {path: 'skill-page', component: SkillPageComponent}
+  {path: 'skill-page', component: SkillPageComponent},
+  {path: '**', component: PageNotFoundComponent}
 ];
