75d94fb0695873d32c3a89ff31adadb00b9d827d
Paco Poyo-Dufour <96145167+PoyPoyyyy@users.noreply.github.com>
2025-01-28 09:11:50 +0100

    Fix - routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import {EmployeePageComponent} from './components/employee/employee-page/employe
 import {MissionPageComponent} from './components/mission/mission-page/mission-page.component';
 import {SkillPageComponent} from './components/skill/skill-page/skill-page.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import {EmployeeFormUpdateComponent} from './components/employee/employee-form-update/employee-form-update.component';
 
 export const routes: Routes = [
-  
   {path: 'employee-page', component: EmployeePageComponent},
   {path: 'mission-page', component: MissionPageComponent},
   {path: 'skill-page', component: SkillPageComponent},
+  {path: 'employee/edit/:id', component: EmployeeFormUpdateComponent},
+  {path: 'index', redirectTo: '/', pathMatch: 'full'},
   {path: '', redirectTo: 'employee-page', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
 ];
