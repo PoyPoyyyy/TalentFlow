6ecd0c2df7223f99116bdc9c930b3c223e980778
Paco Poyo-Dufour <96145167+PoyPoyyyy@users.noreply.github.com>
2025-02-27 18:14:03 +0100

    Add - logs files

---
+++ frontend/src/app/components/log/log-page/log-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-page',
+  imports: [],
+  templateUrl: './log-page.component.html',
+  styleUrl: './log-page.component.css'
+})
+export class LogPageComponent {
+
+}
