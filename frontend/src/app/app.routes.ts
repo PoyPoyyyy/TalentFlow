7fccc87c70ed35a4a12133140f1f891e3d719059
Timothé Renard <tim.renard31@gmail.com>
2025-03-16 12:55:03 +0100

    Update - Welcome-page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,14 +7,16 @@ import {EmployeeFormUpdateComponent} from './components/employee/employee-form-u
 import {SkillFormUpdateComponent} from './components/skill/skill-form-update/skill-form-update.component';
 import { MissionFormAddComponent } from './components/mission/mission-form-add/mission-form-add.component';
 import { MissionFormUpdateComponent } from './components/mission/mission-form-update/mission-form-update.component';
+import { WelcomePageComponent } from './components/welcome/welcome-page/welcome-page.component';
 
 export const routes: Routes = [
   {path: 'employee-page', component: EmployeePageComponent},
   {path: 'mission-page', component: MissionPageComponent},
   {path: 'skill-page', component: SkillPageComponent},
+  {path: 'welcome-page', component: WelcomePageComponent},
   {path: 'employee/edit/:id', component: EmployeeFormUpdateComponent},
   {path: 'mission/edit/:id', component: MissionFormUpdateComponent},
-  {path: 'index', redirectTo: '/', pathMatch: 'full'},
+  {path: 'index', redirectTo: '/welcome-page', pathMatch: 'full'},
   {path: 'skill/edit/:code', component: SkillFormUpdateComponent},
   {path: '', redirectTo: 'employee-page', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
